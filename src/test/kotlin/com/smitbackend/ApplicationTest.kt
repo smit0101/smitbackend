db29080e36f69cec1d1cb295be3da51d96c91921
smitt0101 <smitkalkani9099@gmail.com>
2023-09-13 01:33:59 -0400

    First commit

--- src/test/kotlin/com/smitbackend/ApplicationTest.kt
+++ src/test/kotlin/com/smitbackend/ApplicationTest.kt
@@ -1,14 +1,20 @@
 package com.smitbackend
 
 import com.smitbackend.plugins.*
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.client.utils.EmptyContent.status
-import io.ktor.http.*
-import io.ktor.server.testing.*
-import junit.framework.TestCase.assertEquals
-import org.junit.Test
 import kotlin.test.*
+import io.ktor.server.testing.*
+import org.junit.Test
+
 
 class ApplicationTest {
     @Test
@@ -16,9 +22,6 @@ class ApplicationTest {
         application {
             configureRouting()
         }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
+
     }
 }
