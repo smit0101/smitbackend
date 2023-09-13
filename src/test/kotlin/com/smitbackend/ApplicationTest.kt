92a97419206bd70cd26b597d073b94affc2c60bf
smitt0101 <smitkalkani9099@gmail.com>
2023-09-13 00:46:26 -0400

    First commit

--- src/test/kotlin/com/smitbackend/ApplicationTest.kt
+++ src/test/kotlin/com/smitbackend/ApplicationTest.kt
@@ -3,8 +3,11 @@ package com.smitbackend
 import com.smitbackend.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.client.utils.EmptyContent.status
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
 import kotlin.test.*
 
 class ApplicationTest {
