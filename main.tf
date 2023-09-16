6b08fd574437e65cefcaac8e24a5b38da6677913
smitt0101 <smitkalkani9099@gmail.com>
2023-09-16 01:50:52 -0400

    This application is a simple Ktor backend service which connects to MONGODB cluster and connection url is set as an env variable and in the github actions it is set as secrets and workflow file has four steps defined repository checkout and the setting evn varibale for mongodb connection and Building of docker image and the running the container.
    In this commit I did authentication to docker docker hub set password and username as a secrets and set env in workflow file and pushing and image to docker hub repository,   Added terraform and k8s Deployment and Service

---
+++ main.tf
@@ -0,0 +1,63 @@
+variable "google_credentials_file" {
+  description = "Path to the Google Cloud service account key file"
+  default     = "secrets/smitbackend-05508106981c.json"
+}
+
+variable "project_id" {
+  description = "Google Cloud project ID"
+  default     = "smitbackend"
+}
+
+variable "region" {
+  description = "GCP region for the cluster"
+  default     = "northamerica-northeast2"
+}
+
+provider "google" {
+  credentials = file(var.google_credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_container_cluster" "my_cluster" {
+  name               = "my-gke-cluster"
+  location           = var.region
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  node_pool {
+    name       = "default-pool"
+    machine_type = "e2-medium"
+    initial_node_count = 1
+  }
+}
+
+resource "google_container_node_pool" "default" {
+  name       = "default-pool"
+  location   = var.region
+  cluster    = google_container_cluster.my_cluster.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
+
+resource "google_container_node_pool" "nodes" {
+  name       = "nodes-pool"
+  location   = var.region
+  cluster    = google_container_cluster.my_cluster.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
