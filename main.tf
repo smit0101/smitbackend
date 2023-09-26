17873b5d23114678413d02f2dda55a45cdc6346c
smit0101 <smitkalkani9099@gmail.com>
2023-09-26 05:41:05 -0400

    Added Docker build and push to gcp container registry steps

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "project_id" {
 
 variable "region" {
   description = "GCP region for the cluster"
-  default     = "us-cental1-a"
+  default     = "us-central1-a"
 }
 
 provider "google" {
