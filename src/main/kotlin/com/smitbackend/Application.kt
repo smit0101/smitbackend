04d69e80ec396326ad2d1204952d2de95a152fa6
smit0101 <smitkalkani9099@gmail.com>
2023-09-27 04:54:11 -0400

    worked on terraform

--- src/main/kotlin/com/smitbackend/Application.kt
+++ src/main/kotlin/com/smitbackend/Application.kt
@@ -6,7 +6,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "", module = Application::module)
         .start(wait = true)
 }
 
