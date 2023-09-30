ae891b386524be0bd566cdbdf56ddc5ce918a567
smit0101 <smitkalkani9099@gmail.com>
2023-09-30 09:37:16 -0400

    Let see if it works

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ provider "google" {
 resource "google_container_cluster" "my_cluster" {
   name               = "my-gke-cluster"
   location           = var.region
+  remove_default_node_pool = true
   initial_node_count = 1
 }
 
